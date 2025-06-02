b75805555e10bbcffe095cb4d190b3cd0dd92d6f
Minh Thang <ThangNM.B22DCVT525@stu.ptit.edu.vn>
2025-06-02 21:40:36 +0700

    add RecipeItem with Item

--- frontend/food-blog-app/src/components/RecipeItems.jsx
+++ frontend/food-blog-app/src/components/RecipeItems.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
+import {useLoaderData} from "react-router-dom";
+import foodRecipe from '../assets/foodRecipe.jpg'
+import {BsStopwatchFill} from "react-icons/bs";
+import {FaHeart} from "react-icons/fa";
 
 
 export default function RecipeItems() {
+    const allRecipes = useLoaderData();
     return (
-        <div>
-            RecipeItems
+        <div className='card-container'>
+            {
+                allRecipes?.map((item, index) => {
+                    return(
+                        <div key={index} className='card'>
+                            <img src={`${foodRecipe}`} alt="" width="120px" height="100px"/>
+                            <div className="card-body">
+                                <div className="title">{item.title}</div>
+                            </div>
+                            <div className='icons'>
+                                <div className='timer'><BsStopwatchFill/>30min</div>
+                                <FaHeart />
+                            </div>
+                        </div>
+                    )
+                })
+            }
         </div>
     )
 }
