37b202740eb8acf6378967f1b022a380ce978ade
Minh Thang <ThangNM.B22DCVT525@stu.ptit.edu.vn>
2025-06-02 15:37:22 +0700

    Cấu hình Router

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,7 @@ const app = express();
 app.use(express.json());
 const dotenv = require('dotenv').config();
 const port = process.env.PORT;
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-})
+
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 })
