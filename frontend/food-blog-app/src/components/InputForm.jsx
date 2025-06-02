9d516ff1e4a0c3239796e75a39337493506218c8
Minh Thang <ThangNM.B22DCVT525@stu.ptit.edu.vn>
2025-06-02 23:39:26 +0700

    Thêm sự kiện chưa có accout bấm create accout chuyển sang sign up và bấm already have accout bên signup để chuyển sang signin

--- frontend/food-blog-app/src/components/InputForm.jsx
+++ frontend/food-blog-app/src/components/InputForm.jsx
@@ -1,19 +1,33 @@
-import React from "react"
+import React ,{useState} from "react"
 export default function InputForm() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [signUp, setSignUp] = useState("");
+    const handleOnSubmit = (e) => {
+        e.preventDefault();
+        let endpoint = (signUp) ? "signUp" : "login";
+    }
     return (
         <>
-            <form className='form'>
+            <form className='form' onSubmit={handleOnSubmit}>
                 <div className='form-control'>
                     <label>Email</label>
-                    <input type='email' className='input' required />
+                    <input type='email' className='input'
+                           onChange={(e) => setEmail(e.target.value)}
+                           required />
                 </div>
                 <div className='form-control'>
                     <label>Password</label>
-                    <input type='password' className='input' required />
+                    <input type='password' className='input'
+                           onChange={(e) => setPassword(e.target.value)}
+                           required />
                 </div>
-                <button type='submit'>Login</button>
+                <button type='submit'>{(signUp) ? "Sign Up" :"Login"}</button>
                 <br/>
-                <p>Create New Account</p>
+                <p onClick={() => setSignUp(prevState =>!prevState)}>{(signUp)
+                    ? "Already have an account"
+                    :"Create New Account"}
+                </p>
             </form>
         </>
     )
