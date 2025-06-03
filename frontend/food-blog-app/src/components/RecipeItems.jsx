c7c938a27cfc2161ef60dc4d8ae1b847ce3bd928
Minh Thang <ThangNM.B22DCVT525@stu.ptit.edu.vn>
2025-06-03 19:29:42 +0700

    hoàn thiện hàm xóa recipe

--- frontend/food-blog-app/src/components/RecipeItems.jsx
+++ frontend/food-blog-app/src/components/RecipeItems.jsx
@@ -1,4 +1,4 @@
-import React ,{useState}from 'react'
+import React, {useEffect, useState} from 'react'
 import {Link, useLoaderData} from "react-router-dom";
 import foodRecipe from '../assets/foodRecipe.jpg'
 import {BsStopwatchFill} from "react-icons/bs";
@@ -7,20 +7,25 @@ import {FaEdit} from "react-icons/fa";
 import {MdDelete} from "react-icons/md";
 import axios from "axios";
 export default function RecipeItems() {
-    const allRecipes = useLoaderData();
+    const recipes = useLoaderData();
     let path = window.location.pathname==="/myRecipe" ? true : false;
-    const [allRecipe, setAllRecipe] = useState(allRecipes);
+    const [allRecipes, setAllRecipes] = useState();
+    console.log(allRecipes)
+    useEffect(()=>{
+        setAllRecipes(recipes)
+    },[recipes])
     const onDelete = async (id) => {
         console.log(id);
         await axios.delete(`http://localhost:5000/recipe/${id}`)
             .then(res => {
                 console.log(res)
             })
+        setAllRecipes(recipes=>recipes.filter(recipe=>recipe.id !== id))
     }
     return (
         <div className='card-container'>
             {
-                allRecipes?.map((item, index) => {
+                recipes?.map((item, index) => {
                     return(
                         <div key={index} className='card'>
                             <img src={`http://localhost:5000/images/${item.coverImage}`} alt="" width="120px" height="100px"/>
