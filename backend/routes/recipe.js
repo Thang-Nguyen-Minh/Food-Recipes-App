d497966a2f4a7b39e4377951987e89ecda686223
Minh Thang <ThangNM.B22DCVT525@stu.ptit.edu.vn>
2025-06-03 16:50:34 +0700

    upload ảnh và khi bấm add vào sẽ hiện ra màn hình chính

--- backend/routes/recipe.js
+++ backend/routes/recipe.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const {getRecipes,getRecipeById,addRecipe,editRecipe,deleteRecipe} = require("../controller/recipe");
+const {getRecipes,getRecipe,addRecipe,editRecipe,deleteRecipe,upload} = require("../controller/recipe");
 const app = express();
 const router = express.Router();
 router.get("/",getRecipes)//Get All Recipe
-router.get("/:id",getRecipeById)//Get Recipe By Id
-router.post("/",addRecipe)// add recipe
+router.get("/:id",getRecipe)//Get Recipe By Id
+router.post("/",upload.single("file"),addRecipe)// add recipe
 router.put("/:id",editRecipe)// edit recipe
 router.delete("/:id",deleteRecipe)//Delete recipe
 
--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,19 @@
-const express = require('express');
-const app = express();
-const cors = require("cors");
-app.use(cors());
-app.use(express.json());
-const dotenv = require('dotenv').config();
-const connectDb=require("./config/connectionDb.js");
-connectDb();
-const port = process.env.PORT;
-const recipesRoutes = require('./routes/recipe');
-const userRoutes=require("./routes/user");
+const express=require("express")
+const app=express()
+const dotenv=require("dotenv").config()
+const connectDb=require("./config/connectionDb")
+const cors=require("cors")
 
-app.use("/",userRoutes);
-app.use("/recipe", recipesRoutes);
+const PORT=process.env.PORT || 3000
+connectDb()
 
-app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
+app.use(express.json())
+app.use(cors())
+app.use(express.static("public"))
+
+app.use("/",require("./routes/user"))
+app.use("/recipe",require("./routes/recipe"))
+
+app.listen(PORT,(err)=>{
+    console.log(`app is listening on port ${PORT}`)
 })
