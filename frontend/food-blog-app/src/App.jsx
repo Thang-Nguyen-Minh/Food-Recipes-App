6dfdd4b0793ae30c4670fced70788f4ac31797fe
Minh Thang <ThangNM.B22DCVT525@stu.ptit.edu.vn>
2025-06-03 20:18:25 +0700

    Hoàn thiện app.jsx và thêm sự kiện khi xóa khỏi mục yêu thích item, khó vl chứ đùa đâu nhưng thôi chắc cố đc

--- frontend/food-blog-app/src/App.jsx
+++ frontend/food-blog-app/src/App.jsx
@@ -12,17 +12,21 @@ const getAllRecipes=async()=>{
     })
     return allRecipes
 }
+
 const getMyRecipes=async()=>{
     let user=JSON.parse(localStorage.getItem("user"))
     let allRecipes=await getAllRecipes()
     return allRecipes.filter(item=>item.createdBy===user._id)
 }
 
+const getFavRecipes=()=>{
+    return JSON.parse(localStorage.getItem("fav"))
+}
 const router = createBrowserRouter([
   {path:'/',element:<MainNavigation/>,children:[
       {path:'/',element:<Home/>,loader: getAllRecipes},
       {path:'/myRecipe',element:<Home/>,loader:getMyRecipes},
-      {path:'/favRecipe',element:<Home/>},
+      {path:'/favRecipe',element:<Home/>, loader:getFavRecipes},
       {path:'/addRecipe',element:<AddFoodRecipe/>},
       {path:'/editRecipe/:id',element:<EditRecipe/>},
   ]}
